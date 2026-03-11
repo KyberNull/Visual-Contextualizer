35e59c93ceecc0ae331670ed56458572fbe79686
Aasish <50045306+KyberNull@users.noreply.github.com>
2026-03-11 20:29:19 +0000

    Integrate llama.cpp

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    tauri_build::build()
+    tauri_build::build();
+
+    let dst = cmake::Config::new("llama_cpp")
+        .define("LLAMA_BUILD_TESTS", "OFF")
+        .define("LLAMA_BUILD_EXAMPLES", "OFF")
+        .build();
+
+    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
+    println!("cargo:rustc-link-lib=llama");
+
+    let bindings = bindgen::Builder::default()
+        .header("llama_cpp/include/llama.h")
+        .clang_arg(format!("-I{}", dst.join("include").display()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings.write_to_file(out.join("bindings.rs")).unwrap();
 }
