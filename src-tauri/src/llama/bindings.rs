7cc868082953ec2c68a8b528d70d0bb3ad810eb9
Aasish <50045306+KyberNull@users.noreply.github.com>
2026-03-13 14:29:21 +0000

    Removed style warnings from autogenerated bindings

--- src-tauri/src/llama/bindings.rs
+++ src-tauri/src/llama/bindings.rs
@@ -1,1 +1,8 @@
+#![allow(
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals,
+    dead_code,
+    improper_ctypes
+)]
 include!(concat!(env!("OUT_DIR"), "/llama_bindings.rs"));
