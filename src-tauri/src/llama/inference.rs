d8a8e98857f6b14d2bd5c054fc0ccd95a2a673ee
Aasish <50045306+KyberNull@users.noreply.github.com>
2026-03-14 06:15:10 +0000

    Implement system prompt (fixes template leaking into output)

--- src-tauri/src/llama/inference.rs
+++ src-tauri/src/llama/inference.rs
@@ -8,6 +8,33 @@ use crate::llama::model::LlamaModel;
 
 // TODO: make mlock() true based on available ram
 
+const SYSTEM_PROMPT: &str = ""; /*"You are a visual accessibility assistant.
+
+Your job is to explain what is happening in the given image to a visually impaired user.
+
+Focus on the visual meaning, not pixel details.
+
+Process the image in three steps:
+1. Identify the type of visual content (chart, UI panel, table, text block, diagram, etc).
+2. Identify the most important visual elements.
+3. Describe the key insight or pattern.
+
+If the image contains a graph or chart:
+- identify the chart type
+- describe the main trend or comparison
+- ignore decorative elements
+
+Write in spoken language suitable for text-to-speech narration.
+Avoid symbols, bullet points, and abbreviations.
+
+Focus on the trend, comparison, or message of the visual.
+Ignore colors, styling, or decorative UI elements unless they matter.
+
+Do not output bullet points, headings, or numbered lists.
+Write 3-4 sentences in normal conversational language.
+Be descriptive but concise.
+Do not guess details that are not visible."; */
+
 pub struct ContextConfig {
     pub n_ctx: u32,
     pub n_batch: u32,
@@ -103,98 +130,106 @@ impl LlamaPipeline {
         })
     }
     pub fn generate(&mut self, prompt: &str, cfg: &GenerationConfig) -> Result<String, String> {
-    unsafe { llama_memory_clear(llama_get_memory(self.ctx), true) };
-    self.reset_sampler(cfg);
-    
-    let text_prompt = template::Message {
-        role: template::Role::User,
-        content: vec![template::Content::Text(prompt.to_string())],
-    };
-    let prompt = vec![text_prompt];
-    let formatted_prompt = template::render(&prompt);
-    let prompt_tokens = self.model.tokenize(&formatted_prompt, true)?;
-    if prompt_tokens.is_empty() {
-        return Err("Prompt tokenization produced no tokens".to_string());
-    }
 
-    let n_prompt = prompt_tokens.len();
-    let eos = self.model.eos_token();
+        // Clear old context
+        unsafe { llama_memory_clear(llama_get_memory(self.ctx), true) };
+        self.reset_sampler(cfg);
+        
+        let system_prompt = template::Message {
+            role: template::Role::System,
+            content: vec![template::Content::Text(SYSTEM_PROMPT.to_string())],
+        };
 
+        let user_prompt = template::Message {
+            role: template::Role::User,
+            content: vec![template::Content::Text(prompt.to_string())],
+        };
 
-    // ---- build batch for prompt ----
-
-    //llama_batch_init allocates the memory based on the nos of tokens passed in it.
-    let mut batch = unsafe { llama_batch_init(n_prompt as i32, 0, 1) };
-    //MANUALLY setting the nos of tokens to be processed.
-    batch.n_tokens = n_prompt as i32;
-
-    unsafe {
-        for i in 0..n_prompt {
-            *batch.token.add(i) = prompt_tokens[i];
-            *batch.pos.add(i) = i as i32;
-            *batch.n_seq_id.add(i) = 1;
-
-            let seq_ptr = *batch.seq_id.add(i);
-            *seq_ptr.add(0) = 0;
-
-            *batch.logits.add(i) = if i == n_prompt - 1 { 1 } else { 0 };
-        }
-    }
-
-    // ---- decode prompt ----
-    let rc = unsafe { llama_decode(self.ctx, batch) };
-    if rc != 0 {
-        unsafe { llama_batch_free(batch) };
-        return Err(format!("Prompt decode failed with code {rc}"));
-    }
-
-    unsafe { llama_batch_free(batch) };
-
-    // ---- generation loop ----
-    let mut out = String::new();
-    let mut last_pos = (n_prompt - 1) as i32;
-    let mut token = unsafe { llama_sampler_sample(self.sampler, self.ctx, last_pos )};
-    for _ in 0..cfg.max_tokens {
-
-        // sample next token from logits of last position
-
-        if token == eos {
-            break;
+        let prompt = [system_prompt, user_prompt];
+        let formatted_prompt = template::render(&prompt);
+        let prompt_tokens = self.model.tokenize(&formatted_prompt, true)?;
+        if prompt_tokens.is_empty() {
+            return Err("Prompt tokenization produced no tokens".to_string());
         }
 
-        out.push_str(&self.model.token_to_piece(token)?);
+        let n_prompt = prompt_tokens.len();
+        let eos = self.model.eos_token();
 
-        unsafe { llama_sampler_accept(self.sampler, token) };
 
-        // ---- decode generated token ----
-        let mut batch = unsafe { llama_batch_init(1, 0, 1) };
-        batch.n_tokens = 1; // <-- Tell the batch it contains 1 token
+        // ---- build batch for prompt ----
+
+        //llama_batch_init allocates the memory based on the nos of tokens passed in it.
+        let mut batch = unsafe { llama_batch_init(n_prompt as i32, 0, 1) };
+        //MANUALLY setting the nos of tokens to be processed.
+        batch.n_tokens = n_prompt as i32;
 
         unsafe {
-            *batch.token.add(0) = token;
-            *batch.pos.add(0) = last_pos + 1;
-            *batch.n_seq_id.add(0) = 1;
+            for i in 0..n_prompt {
+                *batch.token.add(i) = prompt_tokens[i];
+                *batch.pos.add(i) = i as i32;
+                *batch.n_seq_id.add(i) = 1;
 
-            let seq_ptr = *batch.seq_id.add(0);
-            *seq_ptr.add(0) = 0;
+                let seq_ptr = *batch.seq_id.add(i);
+                *seq_ptr.add(0) = 0;
 
-            *batch.logits.add(0) = 1;
+                *batch.logits.add(i) = if i == n_prompt - 1 { 1 } else { 0 };
+            }
         }
 
+        // ---- decode prompt ----
         let rc = unsafe { llama_decode(self.ctx, batch) };
+        if rc != 0 {
+            unsafe { llama_batch_free(batch) };
+            return Err(format!("Prompt decode failed with code {rc}"));
+        }
+
         unsafe { llama_batch_free(batch) };
 
-        if rc != 0 {
-            return Err(format!("Decode failed while generating with code {rc}"));
+        // ---- generation loop ----
+        let mut out = String::new();
+        let mut last_pos = (n_prompt - 1) as i32;
+        let mut token = unsafe { llama_sampler_sample(self.sampler, self.ctx, last_pos )};
+        for _ in 0..cfg.max_tokens {
+
+            // sample next token from logits of last position
+
+            if token == eos {
+                break;
+            }
+
+            out.push_str(&self.model.token_to_piece(token)?);
+
+            unsafe { llama_sampler_accept(self.sampler, token) };
+
+            // ---- decode generated token ----
+            let mut batch = unsafe { llama_batch_init(1, 0, 1) };
+            batch.n_tokens = 1; // <-- Tell the batch it contains 1 token
+
+            unsafe {
+                *batch.token.add(0) = token;
+                *batch.pos.add(0) = last_pos + 1;
+                *batch.n_seq_id.add(0) = 1;
+
+                let seq_ptr = *batch.seq_id.add(0);
+                *seq_ptr.add(0) = 0;
+
+                *batch.logits.add(0) = 1;
+            }
+
+            let rc = unsafe { llama_decode(self.ctx, batch) };
+            unsafe { llama_batch_free(batch) };
+
+            if rc != 0 {
+                return Err(format!("Decode failed while generating with code {rc}"));
+            }
+
+            last_pos += 1;
+            token = unsafe { llama_sampler_sample(self.sampler, self.ctx, 0)};
+
         }
 
-        last_pos += 1;
-        token = unsafe { llama_sampler_sample(self.sampler, self.ctx, 0)};
-
+        Ok(out)
     }
-
-    Ok(out)
-}
 
 // TODO: Improve sampling strategy
     fn reset_sampler(&mut self, cfg: &GenerationConfig) {
